fa50407f8bd9a96295841901404d7d072bf197f3
Eniola Hannah <heniola395@gmail.com>
2022-09-06 16:40:42 +0100

    Created Function registerNin() and everything therein

--- ussd.py
+++ ussd.py
@@ -54,4 +54,48 @@ class USSD:
             print("Invalid input")
             self.mainMenu()
 
+            
+    def registerNin(self):
+        self.user1 = input("""
+        1. Enter NIN
+        2. Check NIN status
+        3. Main Menu
+        0. Cancel
+    >> """)   
+        if self.user1 == "1":
+            time.sleep(1)
+            self.user2 = input("Enter your 11 digit NIN...\n And make sure it is not less or greater than 11 digit \n>> ")   
+            if len(self.user2) == 11:
+                print("USSD code running...")
+                time.sleep(2)
+                print("you have successfully submitted your NIN, we will send you an SMS shortly after your NIN has been verified") 
+                time.sleep(1)
+            elif len(self.user2) < 11:
+                time.sleep(2)
+                print("Dear Subscriber, you have input an incomplete NIN")
+                self.registerNin()
+            elif len(self.user2) > 11:
+                time.sleep(2)
+                print("Dear Subscriber, you have input more than the 11 digit NIN number")
+                self.registerNin()
+            else:
+                time.sleep(1)
+                print("Invalid Input") 
+                self.registerNin()
+        elif self.user1 == "2":
+            print("USSD code running...")
+            time.sleep(2)
+            print("Dear Subscriber you are yet to submit your NIN")
+        elif self.user1 == "3":
+            print("USSD code running...")
+            time.sleep(1)
+            self.mainMenu()
+        elif self.user1 == "0":
+            print("Thanks for choosing GLO")
+            time.sleep(1)
+            sys.exit()
+        else:
+            print("WRONG INPUT!")
+            self.registerNin()
+            
 glo = USSD()
